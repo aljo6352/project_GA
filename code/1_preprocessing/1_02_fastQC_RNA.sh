016f930f98d38be2af2d4acc9f4c78e911066714
Alex Johansson <aljo6352@pelle2.uppmax.uu.se>
2026-04-13 15:00:18 +0200

    Added script for trimmed RNA fastQC

---
+++ code/1_preprocessing/1_02_fastQC_RNA.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -n 1
+#SBATCH -t 00:40:00
+#SBATCH -J 1_02_fastQC_RNA
+#SBATCH --mail-type=ALL
+
+#load modules
+
+module load FastQC/0.12.1-Java-17
+
+#define variables/paths
+
+INPUT_PATH="/home/aljo6352/genome_analysis/project_GA/data/processed_data/1_01_trimmed_RNA"
+
+OUTPATH="/home/aljo6352/genome_analysis/project_GA/analyses_outputs/1_preprocessing/1_02_fastQC_RNA/"
+
+#run
+for  file in $INPUT_PATH/*_?_R?_paired.fq.gz
+do
+fastqc $file -o $OUTPATH
+done
