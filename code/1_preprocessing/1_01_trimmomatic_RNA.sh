19e8c0b490ff45b9f2ee631f68d561ee230264f3
Alex Johansson <aljo6352@pelle1.uppmax.uu.se>
2026-04-15 15:54:41 +0200

    added script for canu + modified trimmomatic to work in nobackup folder

--- code/1_preprocessing/1_01_trimmomatic_RNA.sh
+++ code/1_preprocessing/1_01_trimmomatic_RNA.sh
@@ -12,26 +12,20 @@ module load Trimmomatic/0.39-Java-17
 
 #define variables/paths
 
-TRIMMOMATIC="/sw/arch/eb/software/Trimmomatic/0.39-Java-17"
-INPUT_PATH="/home/aljo6352/genome_analysis/project_GA/data/raw_data"
-OUTPUT_PATH="/home/aljo6352/genome_analysis/project_GA/data/processed_data/1_01_trimmed_RNA"
+TRIMMOMATIC=/sw/arch/eb/software/Trimmomatic/0.39-Java-17
+INPUT_PATH=/home/aljo6352/genome_analysis/project_GA/data/raw_data
+OUTPUT_PATH=/proj/uppmax2026-1-61/nobackup/work/aljo6352/1_01_trimmomatic_RNA
 
 
 #run
-for i in 1 2 3
+for u in control treated
 do
-java -jar $TRIMMOMATIC/trimmomatic-0.39.jar PE $INPUT_PATH/transcriptome_treated_${i}_R1.fq.gz $INPUT_PATH/transcriptome_treated_${i}_R2.fq.gz \
-$OUTPUT_PATH/treated_${i}_R1_paired.fq.gz $OUTPUT_PATH/treated_${i}_R1_unpaired.fq.gz \
-$OUTPUT_PATH/treated_${i}_R2_paired.fq.gz $OUTPUT_PATH/treated_${i}_R2_unpaired.fq.gz \
-ILLUMINACLIP:$TRIMMOMATIC/adapters/TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
-wait
-done
-
-for i in 1 2 3
-do
-java -jar $TRIMMOMATIC/trimmomatic-0.39.jar PE $INPUT_PATH/transcriptome_control_${i}_R1.fq.gz $INPUT_PATH/transcriptome_control_${i}_R2.fq.gz \
-$OUTPUT_PATH/control_${i}_R1_paired.fq.gz $OUTPUT_PATH/control_${i}_R1_unpaired.fq.gz \
-$OUTPUT_PATH/control_${i}_R2_paired.fq.gz $OUTPUT_PATH/control_${i}_R2_unpaired.fq.gz \
-ILLUMINACLIP:$TRIMMOMATIC/adapters/TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
-wait
+	for i in 1 2 3
+	do
+	java -jar $TRIMMOMATIC/trimmomatic-0.39.jar PE -threads 2 $INPUT_PATH/transcriptome_${u}_${i}_R1.fq.gz $INPUT_PATH/transcriptome_${u}_${i}_R2.fq.gz \
+	$OUTPUT_PATH/${u}_${i}_R1_paired.fq.gz $OUTPUT_PATH/${u}_${i}_R1_unpaired.fq.gz \
+	$OUTPUT_PATH/${u}_${i}_R2_paired.fq.gz $OUTPUT_PATH/${u}_${i}_R2_unpaired.fq.gz \
+	ILLUMINACLIP:$TRIMMOMATIC/adapters/TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+	wait
+	done
 done
---
+++ code/2_assembly/2_01_canu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -n 2
+#SBATCH -t 30:00:00
+#SBATCH -J 2_01_canu
+#SBATCH --mail-type=ALL
+
+#load modules
+module load canu/2.3-GCCcore-13.3.0-Java-17
+
+#run
+canu -p 2_01_canu -d /proj/uppmax2026-1-61/nobackup/work/aljo6352/2_01_canu \
+genomeSize=16m gridOptions="-A uppmax2026-1-61 -M pelle -t 12:00:00" \
+maxThreads=2 corThreads=2 oeaThreads=2 cnsThreads=2 \
+-nanopore /home/aljo6352/genome_analysis/project_GA/data/raw_data/chr3_nanopore_clean.fq.gz
+
+#make soft links back to home
+cd /home/aljo6352/genome_analysis/project_GA/data/processed_data/2_01_canu
+ln -s /proj/uppmax2026-1-61/nobackup/work/aljo6352/2_01_canu/*fasta*
+cd /home/aljo6352/genome_analysis/project_GA/analyses_outputs/2_assembly/2_01_canu
+ln -s /proj/uppmax2026-1-61/nobackup/work/aljo6352/2_01_canu/*report
