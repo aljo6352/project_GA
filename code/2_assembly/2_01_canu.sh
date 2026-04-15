bbd45469e84ef49391e788fbc1390486c6b0195c
Alex Johansson <aljo6352@pelle2.uppmax.uu.se>
2026-04-15 16:16:57 +0200

    removed ln -s commands from canu script since it caused issues

--- code/2_assembly/2_01_canu.sh
+++ code/2_assembly/2_01_canu.sh
@@ -19,8 +19,3 @@ genomeSize=16m gridOptions="-A uppmax2026-1-61 -M pelle -t 12:00:00" \
 maxThreads=2 corThreads=2 oeaThreads=2 cnsThreads=2 \
 -nanopore /home/aljo6352/genome_analysis/project_GA/data/raw_data/chr3_nanopore_clean.fq.gz
 
-#make soft links back to home
-cd /home/aljo6352/genome_analysis/project_GA/data/processed_data/2_01_canu
-ln -s /proj/uppmax2026-1-61/nobackup/work/aljo6352/2_01_canu/*fasta*
-cd /home/aljo6352/genome_analysis/project_GA/analyses_outputs/2_assembly/2_01_canu
-ln -s /proj/uppmax2026-1-61/nobackup/work/aljo6352/2_01_canu/*report
