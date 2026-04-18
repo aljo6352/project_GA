12072be8661ec5eb3f32c1486b5aeba92aa30700
Alex Johansson <aljo6352@pelle2.uppmax.uu.se>
2026-04-18 02:23:37 +0200

    typo in bwa script

--- code/2_assembly/2_02_bwa.sh
+++ code/2_assembly/2_02_bwa.sh
@@ -14,7 +14,7 @@ bwa index -p /proj/uppmax2026-1-61/nobackup/work/aljo6352/2_02_bwa/2_02_index -a
 
 #then we can use that to run bwa mem algorithm
 bwa mem -t 2 /proj/uppmax2026-1-61/nobackup/work/aljo6352/2_02_bwa/2_02_index \
-/proj/uppmax2026-1-61/nobackup/work/aljo6352/2_01_canu/2_01_canu.contigs.fasta\
+/proj/uppmax2026-1-61/nobackup/work/aljo6352/2_01_canu/2_01_canu.contigs.fasta \
 /home/aljo6352/genome_analysis/project_GA/data/processed_data/1_01_trimmed_DNA/R1_paired.fq.gz \
 /home/aljo6352/genome_analysis/project_GA/data/processed_data/1_01_trimmed_DNA/R2_paired.fq.gz \
 > /proj/uppmax2026-1-61/nobackup/work/aljo6352/2_02_bwa/2_02_bwa.sam
