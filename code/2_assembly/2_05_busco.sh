c1657d93c6f8e314f005387d21fac9c31deb44ab
Alex Johansson <aljo6352@pelle2.uppmax.uu.se>
2026-04-28 13:58:12 +0200

    modified busco script to compare before masking

--- code/2_assembly/2_05_busco.sh
+++ code/2_assembly/2_05_busco.sh
@@ -2,13 +2,16 @@
 #SBATCH -A uppmax2026-1-61
 #SBATCH -p pelle
 #SBATCH -n 2
-#SBATCH -t 01:00:00
+#SBATCH -t 02:00:00
 #SBATCH -J 2_05_busco
 #SBATCH --mail-type=ALL
-
 
 module load BUSCO
 
 busco -i /proj/uppmax2026-1-61/nobackup/work/aljo6352/2_04_repeatmasker/2_04_rm.fasta.masked \
--m genome -c 2 --out /proj/uppmax2026-1-61/nobackup/work/aljo6352/2_05_qc/2_05 \
+-m genome -c 2 --out /proj/uppmax2026-1-61/nobackup/work/aljo6352/2_05_qc/2_05_maskedQC \
+-l embryophyta_odb10
+
+busco -i /proj/uppmax2026-1-61/nobackup/work/aljo6352/2_03_pilon/2_03_pilon.fasta \
+-m genome -c 2 --out /proj/uppmax2026-1-61/nobackup/work/aljo6352/2_05_qc/2_05_unmaskedQC \
 -l embryophyta_odb10
